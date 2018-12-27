5f2e361fd66e9d305f9def78edf70e7272b85e41
Héctor Go. An <hectorgoan@usal.es>
2018-12-27 12:44:10 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+## WhatsPad
+
+![](img/icon.png)
+
+### A WhatsApp (web) client for the iPad.
+
+A simple webview that let's you use the WhatsApp web client on your iPad.
+
+#### Motivation: 
+
+I needed a WhatsApp client for my iPad and all of the ones available in the App Store looked a bit suspicious (strange comments). 
+
+![](img/1.png)
+![](img/2.png)
+![](img/3.png)
+![](img/4.png)
+
+#### Run/Install:
+
+![](img/5.png)
+
+1. Open the project in XCode
+2. Connect your iOS device
+3. Run the project in your device
+
+#### TODO:
+
+- Publish it on the App Store ($$$)
+- Enable notifications
---
+++ WhatsPad/WhatsPad/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  WhatsPad
+//
+//  Created by Héctor Gonzalo Andrés on 27/12/2018.
+//  Copyright © 2018 Héctor Gonzalo Andrés. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+
+class ViewController: UIViewController, WKNavigationDelegate {
+
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+
+        super.viewDidLoad()
+
+        let url = URL(string: "https://web.whatsapp.com/")!
+        webView.customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36"
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+        
+    }
+
+
+}
+
